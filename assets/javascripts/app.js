fae8a635759b149d56fc9e0c1bfc94a3f67cf104
JimmyJ <jimmyj.inbox@gmail.com>
2016-11-22 17:16:18 -0600

    all done except animation

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -11,26 +11,24 @@ function renderButtons () {
 
 	//loop through array of sports and create button for each
 	for (var i=0; i<sports.length; i++) {
-		var a = $('<button>');
-		a.addClass('sportB');
-		a.attr('data-name', sports[i]);
-		a.text(sports[i]);
-		$("#sportsButtons").append(a);
+		if (sports[i]=="") { //skip empty ("") array items
+		} else {
+			var a = $('<button>');
+			a.addClass('sportB');
+			a.addClass('btn');
+			a.addClass('btn-primary');
+			a.attr('data-name', sports[i]);
+			a.attr('style', "margin-left:5px;")
+			a.text(sports[i]);
+			$("#sportsButtons").append(a);
+		};
 	};
 };
-
-	//add user input to sports array and redraw buttons
-	// $('#addSport').on('click', function () {
-	// 	var sportAdd = $('#focusedInput').val().trim();
-	// 	sports.push(sportAdd);
-	// 	renderButtons();
-	// 	$('#sports-form')[0].reset();
-	// 	return false;
-	// });
 
 //draw buttons
 renderButtons();
 
+//function to capture button click
 function clickIt () {
 	$(".sportB").on("click", function(){
 		var sportPick = $(this).data("name");
@@ -42,28 +40,44 @@ function clickIt () {
 			clickIt();
 			return false;
 		} else {
-		var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&"+"q="+sportPick;
-
-		$.ajax({url: queryURL, method: "GET"}).done(function(response){
-			console.log("SportPick "+sportPick)
-			var results = response.data;
-			console.log(results);
-			for (var i=0; i < 10; i++) {
-				var sportDiv = $("<div>");
-        		var rating = results[i].rating;
-        		var p = $("<p>").text("Rating: " + rating);
-        		var sportImage = $("<img>");
-        		sportImage.attr("src", results[i].images.fixed_height_still.url);
-        		sportDiv.prepend(p);
-        		sportDiv.prepend(sportImage);
-       			$("#sportsGifs").prepend(sportDiv);	
-			}
-
-
-		})
-		}
-
+			var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&"+"q="+sportPick;
+			$.ajax({url: queryURL, method: "GET"}).done(function(response){
+				console.log("SportPick "+sportPick)
+				var results = response.data;
+				console.log(results);
+				for (var i=0; i < 10; i++) {
+					var sportDiv = $("<div>");
+	        		var rating = results[i].rating;
+	        		var p = $("<p>").text("Rating: " + rating);
+	        		var sportImage = $("<img>");
+	        		var stillImage = results[i].images.fixed_height_still.url;
+	        		sportImage.attr("src", stillImage);
+	        		sportImage.attr("style", "float: left; margin: 20px 10px 20px 10px;");
+	        		sportImage.attr("class", "gifs");
+	        		sportImage.attr("data-still", results[i].images.fixed_height_still.url);
+	        		sportImage.attr("data-animate", results[i].images.fixed_height.url);
+	        		sportImage.attr("data-state", "still");
+	        		p.attr("style", "float: left; color: white; margin-left: -30%; margin-top: -5px;");
+	        		sportDiv.prepend(p);
+	        		sportDiv.prepend(sportImage);
+	       			$("#sportsGifs").prepend(sportDiv);
+	       			$("sportsGifs").prepend("<BR>");
+				};
+			});
+		};
 	});
 };
 
+function animate () {
+	$(".gifs").on("click", function(){
+		var state = $(this).attr("data-state");
+		if (state=="still") {
+
+		} else {
+
+		}
+	});
+}
+
 clickIt ();
+animate ();
